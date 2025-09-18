231cec5074f1712a88c19ef2637dec1ec465f7f5
Masuk Khan <masukkhanemail@gmail.com>
2025-09-18 15:04:11 +0530

    login refactoring

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -68,40 +68,39 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async(req,res)=>{
-  const { email, password} = req.body;
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
 
   try {
-    if (!email || !password) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
+    const user = await User.findOne({ email });
 
-    const user = await User.findOne({email});
-
-    if(!user) return res.status(400).json({message:"Invalid credentials"});
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if(!isPasswordCorrect) return res.status(400).json({message:"Invalid credentials"});
-    generateToken(user._id, res)
-    
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+    generateToken(user._id, res);
+
     res.status(200).json({
       message: "Logged in successfully",
       _id: user._id,
       fullName: user.fullName,
       email: user.email,
-      profilePic: user.profilePic
-    })
-
+      profilePic: user.profilePic,
+    });
   } catch (error) {
     console.error("Error in login controller:", error);
-    res.status(500).json({message:"Internal server error"})
+    res.status(500).json({ message: "Internal server error" });
   }
+};
 
-}
-
-export const logout = (_,res) =>{
-res.cookie("jwt","",{
-  maxAge:0
-});
-res.status(200).json({message:"Logged out successfully"})
-}
+export const logout = (_, res) => {
+  res.cookie("jwt", "", {
+    maxAge: 0,
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
